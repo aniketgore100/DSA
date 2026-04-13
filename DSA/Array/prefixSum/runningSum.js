2e933de229d0f0df2f9de84975b008f6be1b532e
aniketgore100 <goreaniket100@gmail.com>
2026-04-13 12:06:49 +0530

    update

---
+++ DSA/Array/prefixSum/runningSum.js
@@ -0,0 +1,12 @@
+
+
+
+var runningSum = function(nums) {
+    let prefix = [];
+    prefix[0] = nums[0];
+    for(let i = 1; i<nums.length; i++){
+        prefix[i] = prefix[i - 1] + nums[i];
+    }
+    return prefix;
+};
+
